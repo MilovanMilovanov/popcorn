81d7a37e94f0abc2ee142670fb96f8247f9ba1be
Milovan Milovanov <ninapredninazad@gmail.com>
2024-06-11 17:16:09 +0300

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import { render } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import App from "./App";
 
-describe("App Rerendering", () => {
-  test("Check if App is rendered", () => {
+describe("App Rendering", () => {
+  test("App should be rendered", () => {
     const component = render(<App />);
     expect(component).toBeTruthy();
   });
